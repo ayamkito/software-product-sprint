091c16499016764a019652e267963e49100a7754
Kizito Akwotiga <kakwotiga@yahoo.com>
2021-06-11 20:11:22 +0000

    Convert ArrayList<> to Json using Gson Library

--- portfolio/src/main/java/com/google/sps/servlets/FunFactsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FunFactsServlet.java
@@ -1,17 +1,36 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/funfacts")
-public class FunFactsServlet extends HttpServlet{
+public final class FunFactsServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Some interesting Facts coming soon!");
-  }    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        ArrayList<String> funFacts = new ArrayList<String>();
+        funFacts.add("Some interesting facts 0");
+        funFacts.add("Some interesting facts 1");
+
+        // Convert the ArrayList to JSON
+        String json = convertToJsonUsingGson(funFacts);
+
+        // Send the jason data as the response
+        response.setContentType("text/html;");
+        response.getWriter().println(json);
+    }
+
+    // Function converts ArrayList<> to Json
+    private String convertToJsonUsingGson (ArrayList<String> facts) {
+        Gson gson = new Gson();
+        String json = gson.toJson(facts);
+        return json;
+    }
+
 }
